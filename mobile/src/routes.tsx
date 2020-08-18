160d691b7bc802036c0007e8746c0885869e8e18
alexalves <jhosealex@gmail.com>
2020-08-18 10:18:35 -0300

    Teste commit

--- mobile/src/routes.tsx
+++ mobile/src/routes.tsx
@@ -5,7 +5,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import Home from './pages/Home';
 import Points from './pages/Points';
 // import Detail from './pages/Detail';
-
+// teste de commit
 const AppStack = createStackNavigator();
 
 const Routes = () => {
